b8debdb4a02b470f823d0e6e90cb36286d2445e1
Mateus Pinheiro <mateuspinheiro@MacBook-Pro-de-Mateus.local>
2019-09-05 23:08:38 -0300

    Adonis authentication parte one

---
+++ test/functional/session.spec.js
@@ -0,0 +1,30 @@
+const { test, trait } = use('Test/Suite')('Session');
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+//for use typescript in adonis.
+const User = use('App/Models/User');
+
+trait('Test/ApiClient');
+
+test('it should return JWT token when session created', async ({ assert, client }) => {
+  const sessionPayload = {
+    email: 'mateuspinheiro.contato@outlook.com',
+    password: '12345'
+  };
+
+  const user = await Factory
+  .model('App/Models/User')
+  .create(sessionPayload);
+
+  const response = await client
+    .post('/sessions')
+    .send(sessionPayload)
+    .end()
+
+    response.assertStatus(200)
+    assert.exists(response.body.token)
+
+});
